81bd4fceca4d02a06a2a64e1d93e63c57e760de6
Taryn East <git@taryneast.org>
2009-10-17 18:52:45 +0100

    Nicer pickers for wines/suppliers in a listing.
    Added collection selectors so we can pick wine+supplier from a list of
    existing ones instead of hand-typing ids into the field.

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,9 @@
 class Wine < ActiveRecord::Base
+
+
+  # used as a short descriptor eg on the listing page for choosing which
+  # wine we're listing.
+  def display_name
+    [self.name, self.producer].compact.join(' - ')
+  end
 end
