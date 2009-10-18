1b2f7cabc8dfef21e4336ae5971593e771f3448f
Taryn East <git@taryneast.org>
2009-10-18 15:10:20 +0100

    Slightly better affiliate link...

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
   # construct n affiliate link bsed on the supplier's url plus the affiliate
   # code for us, plus the supplier's id for this product
   def affiliate_link
-    "http://#{self.supplier.store_url}#{self.supplier_side_wine_id}&#{self.supplier.affiliate_code}"
+    [self.supplier.store_url, self.supplier_side_wine_id].join('')
   end
 
   def to_xml(options = {})
